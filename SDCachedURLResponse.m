3db0cbf256cff3677feb6bc3bdd93652821d3f8c
Roger So <rogerso@linkomnia.com>
2014-07-25 13:57:28 +0800

    put data in its own key

--- SDCachedURLResponse.m
+++ SDCachedURLResponse.m
@@ -19,23 +19,25 @@
     return [wrappedResponse autorelease];
 }
 
-#pragma mark - NSCopying Methods
+#pragma mark NSCopying Methods
 
 - (id)copyWithZone:(NSZone *)zone
 {
     SDCachedURLResponse *newResponse = [[[self class] allocWithZone:zone] init];
-    if (newResponse) {
+
+    if (newResponse)
+    {
         newResponse.response = [[self.response copyWithZone:zone] autorelease];
     }
+
     return newResponse;
 }
 
-#pragma mark - NSCoding Methods
-
+#pragma mark NSCoding Methods
 - (void)encodeWithCoder:(NSCoder *)coder
 {
     // force write the data of underlying cached response
-    [coder encodeDataObject:self.response.data];
+    [coder encodeObject:self.response.data forKey:@"data"];
     [coder encodeObject:self.response.response forKey:@"response"];
     [coder encodeObject:self.response.userInfo forKey:@"userInfo"];
     [coder encodeInt:self.response.storagePolicy forKey:@"storagePolicy"];
@@ -43,13 +45,14 @@
 
 - (id)initWithCoder:(NSCoder *)coder
 {
-    self = [super init];
-    if (self) {
+    if ((self = [super init]))
+    {
         self.response = [[[NSCachedURLResponse alloc] initWithResponse:[coder decodeObjectForKey:@"response"]
-                                                                  data:[coder decodeDataObject]
+                                                                  data:[coder decodeObjectForKey:@"data"]
                                                               userInfo:[coder decodeObjectForKey:@"userInfo"]
                                                          storagePolicy:[coder decodeIntForKey:@"storagePolicy"]] autorelease];
     }
+
     return self;
 }
 
